ae77e38a29a3e3fc5407e9b4ddf42043ec53360f
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2024-04-25 21:28:57 +0900

    Refactor cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,12 +1,10 @@
-let common = [
-  "test/cucumber/features/**/*.feature",
-  "--require-module ts-node/register", //typescript cucumber
-  "--require ./test/cucumber/features/stepDefinitions/**/*.ts",
-  "--format progress-bar",
-  "--format html:cucumber-report/index.html",
-  `--format-options '{"snippetInterface": "synchronous"}'`,
-].join(" ");
-
 module.exports = {
-  default: common,
+  default: [
+    "test/cucumber/features/**/*.feature",
+    "--require-module ts-node/register", //typescript cucumber
+    "--require ./test/cucumber/features/stepDefinitions/**/*.ts",
+    "--format progress-bar",
+    "--format html:cucumber-report/index.html",
+    `--format-options '{"snippetInterface": "synchronous"}'`,
+  ].join(" "),
 };
